b94ec44d6ef2da74e46ea5f6ad751f318ddcb471
sanjay9 <sanjaymahabir9@gmail.com>
2023-11-18 17:44:07 -0500

    priliminary book model update

--- models/book.js
+++ models/book.js
@@ -1,18 +1,48 @@
 const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
-   
-    bookTitle: {
+
+    title: {
         type: String,
         required: true,
     },
     image: {
         type: String,
         require: false
+    },
+
+    author: {
+        type: String,
+        required: true,
+    },
+    genre: String,
+    publishedDate: Date,
+    ISBN: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    description: String,
+    copiesAvailable: {
+        type: Number,
+        default: 1,
+    },
+    comments :{
+        type: [String],
+        default: []
+    },
+    upvotes: {
+        type: Number,
+        default: 0
+    },
+    downvotes: {
+        type: Number,
+        default: 0
     }
-   
-  
-    });
+
+
+
+});
 
 const Book = mongoose.model("book", bookSchema);
 
