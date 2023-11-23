9c2fd31b4fa4a5963f07f3489bd24bca59f45ff9
sanjay9 <sanjaymahabir9@gmail.com>
2023-11-23 11:40:04 -0500

    Sanjay: Preliminary Drone Delivery Borrow page

---
+++ models/delivery.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        require: false
+    },
+
+    author: {
+        type: String,
+        
+    },
+    genre: String,
+    publishedDate: Date,
+    isbn: {
+        type: String,
+        /*unique: true,*/
+        
+    },
+    description: String,
+    copiesAvailable: {
+        type: Number,
+        default: 1,
+    },
+    weight: Number,
+    comments :{
+        type: Array,
+        default: []
+    },
+    upvotes: {
+        type: Number,
+        default: 0
+    },
+    downvotes: {
+        type: Number,
+        default: 0
+    }
+
+
+
+});
+
+const Book = mongoose.model("book", bookSchema);
+
+module.exports = Book;
--- routes/index.js
+++ routes/index.js
@@ -4,32 +4,30 @@ const Book = require('../models/book')
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  Book.find().sort({ title: 1 , genre: 1}).exec((err, books) => {
+router.get('/', function (req, res, next) {
+  Book.find().sort({ title: 1, genre: 1 }).exec((err, books) => {
     if (err) {
-      res.json({message: err.message});
+      res.json({ message: err.message });
     } else {
-      res.render('homepage', { title: 'BooksOnFly', user: req.user, books: books});      
+      res.render('homepage', { title: 'BooksOnFly', user: req.user, books: books });
     }
   });
-  
+
 });
 
 
-
-
 /* GET Book Details page. */
-router.get('/book-details/:id', function(req, res, next) {
+router.get('/book-details/:id', function (req, res, next) {
 
   let id = req.params.id;
   Book.findById(id, (err, book) => {
-    if(err) {
+    if (err) {
       alert("Book ERROR")
-    } else{
-      if(book == null) {
+    } else {
+      if (book == null) {
         alert("Book not found ERROR")
       } else {
-        res.render('bookdetails', { title: 'Bookdetails', user: req.user, book: book});
+        res.render('bookdetails', { title: 'Bookdetails', user: req.user, book: book });
       }
     }
   })
@@ -38,50 +36,60 @@ router.get('/book-details/:id', function(req, res, next) {
 })
 
 
-
-
-
-router.get('/login', checkNotAuthenticated, function(req, res, next) {
-  res.render('login', { title: 'Login', user: req.user});
+router.get('/login', checkNotAuthenticated, function (req, res, next) {
+  res.render('login', { title: 'Login', user: req.user });
 });
 
-router.get('/register', checkNotAuthenticated, function(req, res, next) {
-  res.render('register', { title: 'Register', user: req.user});
+router.get('/register', checkNotAuthenticated, function (req, res, next) {
+  res.render('register', { title: 'Register', user: req.user });
 });
-
-
-
 
 
 /* POST TO Insert Comments of books*/
-router.post('/comment/:id',checkAuthenticated, function(req, res, next) {
+router.post('/comment/:id', checkAuthenticated, function (req, res, next) {
 
   console.log(req.body.comment);
   var id = req.params.id
- 
-  Book.findByIdAndUpdate(id, 
-    {$push: {comments: { comment: req.body.comment, user_id: req.user._id, userName: req.user.name, date: new Date()} }}, (err, book) => {
-      
-    if(err) {
-      res.json({error:"error posting comment"});
-    } else{
-      res.redirect("/book-details/"+ id);
+
+  Book.findByIdAndUpdate(id,
+    { $push: { comments: { comment: req.body.comment, user_id: req.user._id, userName: req.user.name, date: new Date() } } }, (err, book) => {
+
+      if (err) {
+        res.json({ error: "error posting comment" });
+      } else {
+        res.redirect("/book-details/" + id);
+      }
+    })
+});
+
+
+
+/* Get Delivery page*/
+router.get('/delivery/:id', checkAuthenticated, function (req, res, next) {
+
+   var id = req.params.id
+   
+
+   Book.findById(id, (err, book) => {
+    if (err) {
+      console.log("Book ERROR")
+    } else {
+      if (book == null) {
+        console.log("Book not found ERROR")
+      } else {
+        res.render('delivery', { title: 'Drone Delivery', user: req.user, book: book });
+      }
     }
   })
 
 
-
 });
 
 
 
-
-
-
-
-
-
-
+/*****************************************************
+Middleware Functions
+******************************************************/
 
 
 function checkAuthenticated(req, res, next) {
@@ -102,113 +110,20 @@ function checkNotAuthenticated(req, res, next) {
 
 
 
-/* GET feedback page. */
-router.get('/feedback', function(req, res, next) {
-  res.render('feedback', { title: 'Feedback', user: req.user, success: true});
-});
 
 
 
 /* GET Borrow page. */
-router.get('/borrow', checkAuthenticated, function(req, res, next) {
-  res.render('borrow', { title: 'Borrow', user: req.user});
+router.get('/borrow', checkAuthenticated, function (req, res, next) {
+  res.render('borrow', { title: 'Borrow', user: req.user });
 });
 
 /* GET Return page. */
-router.get('/return', checkAuthenticated, function(req, res, next) {
-  res.render('return', { title: 'Return', user: req.user});
+router.get('/return', checkAuthenticated, function (req, res, next) {
+  res.render('return', { title: 'Return', user: req.user });
 });
 
-/* GET BOOKS index page. */
-router.get('/booklist', function(req, res, next) {
-  res.render('booklist', { title: 'Book List', user: req.user});
-});
 
-router.get('/business', checkAuthenticated, function(req, res, next) {
-  res.render('businessViews', { title: 'Business Contacts', user: req.user});
-});
-
-router.get('/addbookform', checkAuthenticated, function(req, res, next) {
-  res.render('addbookform', { title: 'Add Book Form', user: req.user});
-});
-
-router.get('/businessedit/:id', checkAuthenticated, function(req, res, next) {
-  res.render('businessEdit', { title: 'Business Contacts Edit', _id: req.params.id, user: req.user});
-});
-
-/* GET Services page. */
-router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services', user: req.user});
-});
-
-/* GET Contact Us page. */
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact', user: req.user});
-});
-
-/* secured API for all contacts */
-router.get('/businesscontactsid/:id', checkAuthenticated, async function(req, res, next) {
-  const _id = req.params.id
-  const tempAllContacts = await contactsModel.find({_id})
-  const allContacts = JSON.parse(JSON.stringify(tempAllContacts))
-  res.json(allContacts)
-});
-
-/* secured API for all contacts */
-router.get('/allbusinesscontacts', checkAuthenticated, async function(req, res, next) {
-  const tempAllContacts = await contactsModel.find({}).sort({name: 'asc'})
-  const allContacts = JSON.parse(JSON.stringify(tempAllContacts))
-  res.json(allContacts)
-});
-
-/* secured API for delete contacts */
-router.delete('/deletebusinesscontacts/:id', checkAuthenticated, async function(req, res, next) {
-  const _id = req.params.id
-  try {
-    await contactsModel.deleteOne({_id}, function(err, obj){
-      if (err) throw console.error(err)
-      console.log(_id + " object from mongo deleted.")
-      //Set HTTP method to GET, oTHERWISE WOULD AIM AT DELETE
-      
-      res.redirect(303, '/business')
-    })
-  } catch (e){
-    console.error(e)
-  }
-
-
-});
-
-/* secured API for edit contacts */
-router.post('/editbusinesscontacts/:id', checkAuthenticated, async function(req, res, next) {
-  const _id = req.params.id
-  const name = req.query.name
-  const number = req.query.number
-  const email = req.query.email
-  if (_id == 0){
-    try {
-      const contact = new contactsModel({name, number, email})
-      const result = await contact.save()
-      //console.log(result.id)
-        
-      res.redirect(303, '/business')
-    } catch (e){
-      console.error(e)
-    }  
-  } else {
-    try {
-      await contactsModel.findOneAndUpdate({_id}, {name, number, email}, function(err, obj){
-        if (err) throw console.error(err)
-        console.log({_id, name, number, email} + " object from mongo updated.")
-        //Set HTTP method to GET, oTHERWISE WOULD AIM AT DELETE
-        
-        res.redirect(303, '/business')
-      })
-    } catch (e){
-      console.error(e)
-    }  
-  }
-});
 
 
 
