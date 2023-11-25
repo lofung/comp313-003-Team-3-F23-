104b7f7b3c7edf734c9091d1ea93cd655b16558c
sanjay9 <sanjaymahabir9@gmail.com>
2023-11-25 12:20:27 -0500

    SANJAY: Drone dispatch and display complete
    SANJAY: Drone dispatch update statuses and delivery status complete

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const Book = require('../models/book')
+const Delivery = require('../models/delivery')
+
 const multer = require('multer')
 const fs = require('fs')
 
@@ -8,7 +10,7 @@ const fs = require('fs')
 const upload = multer({ dest: './BookImagesUploaded/' })
 
 /* GET Book management page. */
-router.get('/bookmgmt', function(req, res, next) {
+router.get('/bookmgmt', function (req, res, next) {
   //res.locals.message = "test";
   // Access the query parameter and pass it as a local variable
   const message = req.query.message || "";
@@ -16,23 +18,23 @@ router.get('/bookmgmt', function(req, res, next) {
 
   Book.find().sort({ title: 1 }).exec((err, books) => {
     if (err) {
-      res.json({message: err.message});
+      res.json({ message: err.message });
     } else {
-      res.render('Admin/bookmgmt', { title: 'AdminBooksOnFly', titleBar: 'Book Managment', user: req.user,message, books: books});
+      res.render('Admin/bookmgmt', { title: 'AdminBooksOnFly', titleBar: 'Book Managment', user: req.user, message, books: books });
     }
   });
 
-  
+
 });
 
 /* GET Create Book page. */
-router.get('/create-book', function(req, res, next) {
-  res.render('Admin/addbookform', { title: 'CreateBook', titleBar: 'Create New Book Record', user: req.user});
+router.get('/create-book', function (req, res, next) {
+  res.render('Admin/addbookform', { title: 'CreateBook', titleBar: 'Create New Book Record', user: req.user });
 });
 
 
 /* POST TO INSERT Create Book page. */
-router.post('/create-book', upload.single('image'), function(req, res, next) {
+router.post('/create-book', upload.single('image'), function (req, res, next) {
 
   let fileName = 'noImage.png' //default placeholder image
   if (req.file) {
@@ -51,15 +53,15 @@ router.post('/create-book', upload.single('image'), function(req, res, next) {
     weight: req.body.weight
 
   });
-  book.save((err)=>{
-    if(err){
-      res.json({message: err.message, type: 'danger'});
-    }else {
+  book.save((err) => {
+    if (err) {
+      res.json({ message: err.message, type: 'danger' });
+    } else {
       req.session.message = {
         type: "success",
         message: "Book added succesfully!"
       };
-      
+
       //res.locals.message = "Book added succesfully!";
 
       res.redirect("/admin/bookmgmt?message=Book%20Record%20Created");
@@ -71,17 +73,17 @@ router.post('/create-book', upload.single('image'), function(req, res, next) {
 
 
 /* GET Update Book Form page. */
-router.get('/update-book/:id', function(req, res, next) {
+router.get('/update-book/:id', function (req, res, next) {
 
   let id = req.params.id;
   Book.findById(id, (err, book) => {
-    if(err) {
+    if (err) {
       res.redirect('/admin/bookmgmt?Book%20Update%20Error');
-    } else{
-      if(book == null) {
+    } else {
+      if (book == null) {
         res.redirect("/admin/bookmgmt?Book%20Not%20Found");
       } else {
-        res.render('Admin/editBook', { title: 'UpdateBook', titleBar: 'Update Book Record', user: req.user,book});
+        res.render('Admin/editBook', { title: 'UpdateBook', titleBar: 'Update Book Record', user: req.user, book });
       }
     }
   })
@@ -90,12 +92,12 @@ router.get('/update-book/:id', function(req, res, next) {
 })
 
 /* POST Update Book page. */
-router.post('/update-book/:id', upload.single('image'), function(req, res, next) {
+router.post('/update-book/:id', upload.single('image'), function (req, res, next) {
 
 
 
   let id = req.params.id;
-  
+
 
   //Sanjay, replace old image if new image uploaded.
   let fileName = 'noImage.png' //default placeholder image
@@ -103,7 +105,7 @@ router.post('/update-book/:id', upload.single('image'), function(req, res, next)
     fileName = req.file.filename;
     try {
       fs.unlinkSync("./BookImagesUploaded/" + req.body.old_image);
-    } catch(err){
+    } catch (err) {
       console.log(err);
     }
   } else {
@@ -113,11 +115,12 @@ router.post('/update-book/:id', upload.single('image'), function(req, res, next)
 
   Book.findByIdAndUpdate(id, {
     title: req.body.title,
-    image: fileName}, (err, book) => {
-      
-    if(err) {
+    image: fileName
+  }, (err, book) => {
+
+    if (err) {
       res.redirect('/admin/bookmgmt?Book%20Update%20Error');
-    } else{
+    } else {
       res.redirect("/admin/bookmgmt?message=Book%20Record%20Updated");
     }
   })
@@ -126,24 +129,24 @@ router.post('/update-book/:id', upload.single('image'), function(req, res, next)
 })
 
 /* POST Update Book page. */
-router.get('/delete-book/:id', function(req, res, next) {
+router.get('/delete-book/:id', function (req, res, next) {
 
   let id = req.params.id;
 
   Book.findByIdAndRemove(id, (err, book) => {
-      
-    if(book.image != '' && book.image != 'noImage.png') {
+
+    if (book.image != '' && book.image != 'noImage.png') {
       try {
         fs.unlinkSync("./BookImagesUploaded/" + book.image);
-      } catch(err){
+      } catch (err) {
         console.log(err);
       }
-      
-    } 
 
-    if(err) {
+    }
+
+    if (err) {
       res.redirect('/admin/bookmgmt?Book%20Delete%20Error');
-    } else{
+    } else {
       res.redirect("/admin/bookmgmt?message=Book%20Record%20Deleted");
     }
 
@@ -156,49 +159,85 @@ router.get('/delete-book/:id', function(req, res, next) {
 
 
 
+/* GET home page. */
+router.get('/dronedispatch', async function (req, res, next) {
+
+  const deliveries = await Delivery
+    .find({
+      // user_id: req.user._id, // replace with the actual user_id
+     // status: "0processing", //books for drone dispatch
+      returnDate: { $exists: false }
+    })
+    .populate('user_id')
+    .populate('book_id')
+    //.sort('book_id.title');
+    .sort({ 'delivery.borrowDate': 1 });
+
+  console.log(deliveries);
+
+
+  const returnDeliveries = await Delivery
+    .find({
+      //  user_id: req.user._id, // replace with the actual user_id
+      returnDate: { $exists: true }
+    })
+    .populate('user_id')
+    .populate('book_id')
+    .sort('book_id.title');
+
+
+  res.render('Admin/deliveries', { title: 'Drone Dispatch', titleBar: 'Drone Dispatch', user: req.user, deliveries: deliveries, returnDeliveries: returnDeliveries });
 
 
 
-
-
-
-router.get('/login', checkNotAuthenticated, function(req, res, next) {
-  res.render('login', { title: 'Login', user: req.user});
 });
 
-router.get('/register', checkNotAuthenticated, function(req, res, next) {
-  res.render('register', { title: 'Register', user: req.user});
+
+
+/* POST Update Book page. */
+router.get('/deliveryDispatch/:id/:status', function (req, res, next) {
+
+  let id = req.params.id;
+  let status = req.params.status;
+
+  Delivery.findByIdAndUpdate(id, { $set: { status: status } },
+   (err, delivery) => {
+    if (err) {
+      //alert("Book ERROR")
+      res.send("delivery ERROR");
+    } else {
+
+        res.redirect('/admin/dronedispatch?message=dispatched');
+      
+    }
+  })
+
+})
+
+
+
+
+
+router.get('/login', checkNotAuthenticated, function (req, res, next) {
+  res.render('login', { title: 'Login', user: req.user });
 });
 
-/* GET feedback page. */
-router.get('/feedback', function(req, res, next) {
-  res.render('feedback', { title: 'Feedback', user: req.user, success: true});
+router.get('/register', checkNotAuthenticated, function (req, res, next) {
+  res.render('register', { title: 'Register', user: req.user });
 });
 
 
 
 /* GET Borrow page. */
-router.get('/borrow', checkAuthenticated, function(req, res, next) {
-  res.render('borrow', { title: 'Borrow', user: req.user});
+router.get('/borrow', checkAuthenticated, function (req, res, next) {
+  res.render('borrow', { title: 'Borrow', user: req.user });
 });
 
 /* GET Return page. */
-router.get('/return', checkAuthenticated, function(req, res, next) {
-  res.render('return', { title: 'Return', user: req.user});
+router.get('/return', checkAuthenticated, function (req, res, next) {
+  res.render('return', { title: 'Return', user: req.user });
 });
 
-/* GET BOOKS index page. */
-router.get('/booklist', function(req, res, next) {
-  res.render('booklist', { title: 'Book List', user: req.user});
-});
-
-router.get('/business', checkAuthenticated, function(req, res, next) {
-  res.render('businessViews', { title: 'Business Contacts', user: req.user});
-});
-
-router.get('/addbookform', checkAuthenticated, function(req, res, next) {
-  res.render('addbookform', { title: 'Add Book Form', user: req.user});
-});
 
 
 
